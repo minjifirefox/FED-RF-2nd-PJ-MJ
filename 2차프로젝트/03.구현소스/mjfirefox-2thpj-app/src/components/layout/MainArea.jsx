86040c95ee422f0a35d453e35da6e544209d25b1
zoe <alswl3125@naver.com>
2024-06-17 18:15:46 +0900

    2024.06.17

--- 2차프로젝트/03.구현소스/mjfirefox-2thpj-app/src/components/layout/MainArea.jsx
+++ 2차프로젝트/03.구현소스/mjfirefox-2thpj-app/src/components/layout/MainArea.jsx
@@ -2,6 +2,9 @@
 
 import { Outlet } from "react-router-dom";
 
+// CSS불러오기
+import "../../css/main_area.scss";
+
 // Outlet 컴포넌트는 리액트라우터에서 컴포넌트를
 // 변경하여 출력하는 자리를 잡아주는 역할을 함!
 
--- 2차프로젝트/03.구현소스/mjfirefox-2thpj-app/src/components/pages/Main.jsx
+++ 2차프로젝트/03.구현소스/mjfirefox-2thpj-app/src/components/pages/Main.jsx
@@ -1,11 +1,33 @@
 // 메인 페이지 컴포넌트 ///
 
+// CSS불러오기
+import "../../css/main_area.scss";
+
 export default function Main() {
   //// 코드 리턴구역 //////////////
   return (
     <>
-      <h1>메인영역</h1>
-      <img src="./images/APEX.jpg" alt="로고" />
+      <div className="open">
+        {/* <h1>메인영역</h1> */}
+        <img src="./images/msb_open.gif" alt="오픈" />
+      </div>
+      <div className="intro">
+        <div className="centerlogo">
+          <div className="aaaa">
+            <p className="민지">
+              나는 민지야
+            </p>
+            </div>
+          <div className="bbbb">
+            <img src="./images/msb_logo.jpg" alt="로고" />
+          </div>
+          <div className="cccc">
+          <p className="민지">
+            너도 민지니?
+          </p>
+            </div>
+        </div>
+      </div>
     </>
   );
 } /////////// Main /////////////////////
