0c9380d57ba6b7dbfffc43865190710b15135e2f
zoe <alswl3125@naver.com>
2024-07-24 13:18:50 +0900

    ss

--- 2차프로젝트/03.구현소스/mjfirefox-2thpj-app/src/components/data/ImageGrid.js
+++ 2차프로젝트/03.구현소스/mjfirefox-2thpj-app/src/components/data/ImageGrid.js
@@ -1,9 +1,9 @@
 const images = [
-    { src: process.env.PUBLIC_URL + '/images/tag_banner01 (1).jpg', alt: 'Image 1' },
-    { src: process.env.PUBLIC_URL + '/images/tag_banner01 (2).jpg', alt: 'Image 2' },
-    { src: process.env.PUBLIC_URL + '/images/tag_banner01 (3).jpg', alt: 'Image 3' },
-    { src: process.env.PUBLIC_URL + '/images/tag_banner01 (4).jpg', alt: 'Image 4' },
-    { src: process.env.PUBLIC_URL + '/images/tag_banner01 (5).jpg', alt: 'Image 5' },
-    { src: process.env.PUBLIC_URL + '/images/tag_banner01 (6).jpg', alt: 'Image 6' },
+    { src: process.env.PUBLIC_URL + '/images/tag_banner01_1.jpg', alt: 'Image 1' },
+    { src: process.env.PUBLIC_URL + '/images/tag_banner01_2.jpg', alt: 'Image 2' },
+    { src: process.env.PUBLIC_URL + '/images/tag_banner01_3.jpg', alt: 'Image 3' },
+    { src: process.env.PUBLIC_URL + '/images/tag_banner01_4.jpg', alt: 'Image 4' },
+    { src: process.env.PUBLIC_URL + '/images/tag_banner01_5.jpg', alt: 'Image 5' },
+    { src: process.env.PUBLIC_URL + '/images/tag_banner01_6.jpg', alt: 'Image 6' },
   ];
   
--- 2차프로젝트/03.구현소스/mjfirefox-2thpj-app/src/components/plugin/ImageGrid.jsx
+++ 2차프로젝트/03.구현소스/mjfirefox-2thpj-app/src/components/plugin/ImageGrid.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import '../../css/image_grid.scss';
 
 const images = [
-  { src: process.env.PUBLIC_URL + '/images/tag_banner01 (1).jpg', alt: 'Image 1' },
-  { src: process.env.PUBLIC_URL + '/images/tag_banner01 (2).jpg', alt: 'Image 2' },
-  { src: process.env.PUBLIC_URL + '/images/tag_banner01 (3).jpg', alt: 'Image 3' },
-  { src: process.env.PUBLIC_URL + '/images/tag_banner01 (4).jpg', alt: 'Image 4' },
-  { src: process.env.PUBLIC_URL + '/images/tag_banner01 (5).jpg', alt: 'Image 5' },
-  { src: process.env.PUBLIC_URL + '/images/tag_banner01 (6).jpg', alt: 'Image 6' },
-  { src: process.env.PUBLIC_URL + '/images/tag_banner01 (7).jpg', alt: 'Image 7' },
-  { src: process.env.PUBLIC_URL + '/images/tag_banner01 (8).jpg', alt: 'Image 8' },
-  { src: process.env.PUBLIC_URL + '/images/tag_banner01 (9).jpg', alt: 'Image 9' },
+  { src: process.env.PUBLIC_URL + '/images/tag_banner01_1.jpg', alt: 'Image 1' },
+  { src: process.env.PUBLIC_URL + '/images/tag_banner01_2.jpg', alt: 'Image 2' },
+  { src: process.env.PUBLIC_URL + '/images/tag_banner01_3.jpg', alt: 'Image 3' },
+  { src: process.env.PUBLIC_URL + '/images/tag_banner01_4.jpg', alt: 'Image 4' },
+  { src: process.env.PUBLIC_URL + '/images/tag_banner01_5.jpg', alt: 'Image 5' },
+  { src: process.env.PUBLIC_URL + '/images/tag_banner01_6.jpg', alt: 'Image 6' },
+  { src: process.env.PUBLIC_URL + '/images/tag_banner01_7.jpg', alt: 'Image 7' },
+  { src: process.env.PUBLIC_URL + '/images/tag_banner01_8.jpg', alt: 'Image 8' },
+  { src: process.env.PUBLIC_URL + '/images/tag_banner01_9.jpg', alt: 'Image 9' },
 ];
 
 const ImageGrid = () => {
